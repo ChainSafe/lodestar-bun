0dbfd15695692637e8694d03516b451df2e23a2f
Cayman <caymannava@gmail.com>
2025-09-11 18:41:09 -0400

    feat: add leveldb binding

--- zig/root.zig
+++ zig/root.zig
@@ -3,10 +3,12 @@ const hashtree = @import("hashtree.zig");
 const persistent_merkle_tree = @import("persistent_merkle_tree.zig");
 const bytes = @import("bytes.zig");
 const lmdb = @import("lmdb.zig");
+const leveldb = @import("leveldb.zig");
 
 comptime {
     std.testing.refAllDecls(hashtree);
     std.testing.refAllDecls(persistent_merkle_tree);
     std.testing.refAllDecls(bytes);
     std.testing.refAllDecls(lmdb);
+    std.testing.refAllDecls(leveldb);
 }
