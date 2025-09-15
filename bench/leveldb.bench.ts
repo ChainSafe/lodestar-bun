f1aeb6b3a091eaf4a7c6b31122b3a70a76aa2d39
Cayman <caymannava@gmail.com>
2025-09-15 18:09:44 -0400

    chore: more db benchmarks

--- bench/leveldb.bench.ts
+++ bench/leveldb.bench.ts
@@ -15,63 +15,66 @@ describe("leveldb", () => {
     fs.rmSync(dbPath, {recursive: true, force: true});
   });
 
-  bench({
-    id: "set 10k entries",
-    beforeEach: () => new Uint8Array(1000),
-    fn: (val) => {
-      for (let i = 0; i < 10000; i++) {
-        leveldb.dbPut(db, intToBytes(i, 4, "le"), val);
-      }
-    },
-  });
 
-  bench({
-    id: "batch set 10k entries",
-    beforeEach: () => new Uint8Array(1000),
-    fn: (val) => {
-      const batch = [];
-      for (let i = 0; i < 10000; i++) {
-        batch.push({key: intToBytes(i, 4, "le"), value: val});
-      }
-      leveldb.dbWrite(db, batch);
-    },
-  });
+  for (const n of [1, 10, 100, 1000, 10000]) {
+    bench({
+      id: `set ${n} entries`,
+      beforeEach: () => new Uint8Array(1000),
+      fn: (val) => {
+        for (let i = 0; i < n; i++) {
+          leveldb.dbPut(db, intToBytes(i, 4, "le"), val);
+        }
+      },
+    });
 
-  bench({
-    id: "get 10k entries",
-    before: () => {
-      const val = new Uint8Array(1000);
-      const batch = [];
-      for (let i = 0; i < 10000; i++) {
-        batch.push({key: intToBytes(i, 4, "le"), value: val});
-      }
-      leveldb.dbWrite(db, batch);
-    },
-    fn: () => {
-      for (let i = 0; i < 10000; i++) {
-        leveldb.dbGet(db, intToBytes(i, 4, "le"));
-      }
-    },
-  });
+    bench({
+      id: `batch set ${n} entries`,
+      beforeEach: () => new Uint8Array(1000),
+      fn: (val) => {
+        const batch = [];
+        for (let i = 0; i < n; i++) {
+          batch.push({key: intToBytes(i, 4, "le"), value: val});
+        }
+        leveldb.dbWrite(db, batch);
+      },
+    });
 
-  bench({
-    id: "iterate 10k entries",
-    before: () => {
-      const val = new Uint8Array(1000);
-      const batch = [];
-      for (let i = 0; i < 10000; i++) {
-        batch.push({key: intToBytes(i, 4, "le"), value: val});
-      }
-      leveldb.dbWrite(db, batch);
-    },
-    fn: () => {
-      const iter = leveldb.dbIterator(db);
-      leveldb.iteratorSeekToFirst(iter);
-      for (let i = 0; i < 10000; i++) {
-        leveldb.iteratorKey(iter);
-        leveldb.iteratorNext(iter);
-      }
-      leveldb.iteratorDestroy(iter);
-    },
-  });
+    bench({
+      id: `get ${n} entries`,
+      before: () => {
+        const val = new Uint8Array(1000);
+        const batch = [];
+        for (let i = 0; i < n; i++) {
+          batch.push({key: intToBytes(i, 4, "le"), value: val});
+        }
+        leveldb.dbWrite(db, batch);
+      },
+      fn: () => {
+        for (let i = 0; i < n; i++) {
+          leveldb.dbGet(db, intToBytes(i, 4, "le"));
+        }
+      },
+    });
+
+    bench({
+      id: `iterate ${n} entries`,
+      before: () => {
+        const val = new Uint8Array(1000);
+        const batch = [];
+        for (let i = 0; i < n; i++) {
+          batch.push({key: intToBytes(i, 4, "le"), value: val});
+        }
+        leveldb.dbWrite(db, batch);
+      },
+      fn: () => {
+        const iter = leveldb.dbIterator(db);
+        leveldb.iteratorSeekToFirst(iter);
+        for (let i = 0; i < n; i++) {
+          leveldb.iteratorKey(iter);
+          leveldb.iteratorNext(iter);
+        }
+        leveldb.iteratorDestroy(iter);
+      },
+    });
+  }
 });
