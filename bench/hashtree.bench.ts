7c41e113254d090a379a6e64a8d77526710b54ae
Cayman <caymannava@gmail.com>
2025-09-29 12:09:35 -0400

    chore: tweak benchmarks

--- bench/hashtree.bench.ts
+++ bench/hashtree.bench.ts
@@ -17,22 +17,21 @@ describe("hashtree", () => {
       as.digest(data);
     });
   }
-  bench("digest64 - lodestar-bun", () => {
-    const data = new Uint8Array(64);
-    bun.digest64(data);
-  });
-  bench("digest64 - @chainsafe/as-sha256", () => {
-    const data = new Uint8Array(64);
-    as.digest64(data);
-  });
-  bench("digest2Bytes32 - lodestar-bun", () => {
-    const a = new Uint8Array(32);
-    const b = new Uint8Array(32);
-    bun.digest2Bytes32(a, b);
-  });
-  bench("digest2Bytes32 - @chainsafe/as-sha256", () => {
-    const a = new Uint8Array(32);
-    const b = new Uint8Array(32);
-    as.digest2Bytes32(a, b);
-  });
+  {
+  const data = new Uint8Array(64);
+  const a = new Uint8Array(32);
+  const b = new Uint8Array(32);
+    bench("digest64 - lodestar-bun", () => {
+      bun.digest64(data);
+    });
+    bench("digest64 - @chainsafe/as-sha256", () => {
+      as.digest64(data);
+    });
+    bench("digest2Bytes32 - lodestar-bun", () => {
+      bun.digest2Bytes32(a, b);
+    });
+    bench("digest2Bytes32 - @chainsafe/as-sha256", () => {
+      as.digest2Bytes32(a, b);
+    });
+  }
 });
