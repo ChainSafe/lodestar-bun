af71ca31b147fcce8516c41e0206e44f4120430b
Cayman <caymannava@gmail.com>
2025-10-22 10:31:40 -0400

    chore: add snappy export (#26)

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * as hashtree from "./hashtree.ts";
 export * as leveldb from "./leveldb.ts";
 export * as lmdb from "./lmdb.ts";
 export * as persistentMerkleTree from "./persistent_merkle_tree.ts";
+export * as snappy from "./snappy.ts";
